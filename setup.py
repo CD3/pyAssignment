7701d321946251042cc449c6500473c482c687f2
C.D. Clark III <clifton.clark@gmail.com>
2018-03-21 21:10:57 -0500

    putting version into a file for easy updating.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
+import version
 
 here = path.abspath(path.dirname(__file__))
 
@@ -11,9 +12,10 @@ here = path.abspath(path.dirname(__file__))
     # long_description = f.read()
 long_description= ""
 
+print("version:",version.__version__)
 setup(
     name='pyAssignment',
-    version='0.1.0',
+    version=version.__version__,
     description='A Python module for authoring and assessing homework assignments',
     long_description=long_description,  # Optional
     url='https://github.com/CD3/pyAssignment',
