1f58842bdf9a8df79f272c9f966559aa2c3e163b
C.D. Clark III <clifton.clark@gmail.com>
2018-04-03 21:42:19 -0500

    basic framework for creating and running a set of tests.

--- pyAssignment/Graders/CLGrader.py
+++ pyAssignment/Graders/CLGrader.py
@@ -1,22 +1,68 @@
 from .GraderBase import *
+from ..Utils import Namespace, SFFormatter
 
-import contextlib
+import contextlib, subprocess, io
 
 def run(cmd,**kwargs):
-  pass
 
-class Test(object):
+
+  c = subprocess.run( cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+  r = c.returncode
+  o = c.stdout
+  e = c.stderr
+
+  try:
+    o = o.decode('utf-8')
+    e = e.decode('utf-8')
+  except: pass
+
+  return r,o,e
+
+class ShellTest(object):
   def __init__(self):
+    self._name = ""
     self._cmds = collection()
     self._o = None
+    self._e = None
     self._r = None
 
+    self._namespace = Namespace()
+    self._formatter = SFFormatter()
+
+  @property
+  def NS(self):
+    return self._namespace
+
+  @property
+  def command(self):
+    return ";".join(self._cmds)
+
+  @command.setter
+  def command(self,val):
+    self._cmds = collection()
+    self._cmds.append(val)
+
   def run(self):
-    pass
+    cmd = self._formatter.fmt( ";".join(self._cmds), **self.NS.__dict__ )
+    self._r,self._o,self._e = run(cmd)
+
+
+  @property
+  def returncode(self):
+    return self._r
+
+  @property
+  def output(self):
+    return self._o
+
+  @property
+  def error(self):
+    return self._e
 
   @contextlib.contextmanager
   def add_fail_callback(self):
     pass
+
 
 
 class CLGrader(GraderBase):
@@ -25,9 +71,33 @@ class CLGrader(GraderBase):
     self._tests = collection()
 
   def run(self):
-    pass
+    for t in self._tests:
+      t.run()
+
+  @property
+  def num_tests(self):
+    return len( self._tests )
+
+  @property
+  def num_fail(self):
+    n = 0
+    for t in self._tests:
+      if t.returncode is not None and t.returncode != 0:
+        n += 1
+    return n
+
+  @property
+  def num_success(self):
+    n = 0
+    for t in self._tests:
+      if t.returncode is not None and t.returncode == 0:
+        n += 1
+    return n
+
 
   @contextlib.contextmanager
   def add_test(self):
-    pass
+    t = ShellTest()
+    yield t
+    self._tests.append(t)
 
--- testing/test_Graders.py
+++ testing/test_Graders.py
@@ -1,11 +1,44 @@
+import pytest
+import os
 
-def test_CLGrader():
+from pyAssignment.Graders.CLGrader import CLGrader
 
+def test_CLGrader_simple():
   g = CLGrader()
 
   with g.add_test() as t:
     t.NS.FILE = "file1.txt"
-    t.command = "test -e {FILE}"
+    t.command = "touch {FILE}"
+
+  if os.path.isfile( "file1.txt" ):
+    os.remove("file1.txt")
+  g.run()
+
+  assert os.path.isfile("file1.txt")
+
+def test_CLGrader_tests_summary():
+  g = CLGrader()
+
+  with g.add_test() as t:
+    t.command = "ls"
+  with g.add_test() as t:
+    t.command = "pwd"
+  with g.add_test() as t:
+    t.command = "lkdfs"
+  with g.add_test() as t:
+    t.command = "test 1 -eq 0"
+  with g.add_test() as t:
+    t.command = "ls"
+
+  g.run()
+
+  assert g.num_tests == 5
+  assert g.num_fail == 2
+  assert g.num_success == 3
+
+@pytest.mark.skip()
+def test_CLGrader_multiple_commands():
+  g = CLGrader()
 
   with a.add_test() as t:
     t.NS.DIR = "dir1"
@@ -13,6 +46,10 @@ def test_CLGrader():
 
     t.commands += "test -d {DIR}"
     t.commands += "test -f {DIR}/{FILE}"
+
+@pytest.mark.skip()
+def test_CLGrader_on_fail_extra_commands():
+  g = CLGrader()
 
   with a.add_test() as t:
     t.NS.SCRIPTNAME = "sin.gnuplot"
@@ -24,6 +61,12 @@ def test_CLGrader():
 
     t.on_fail = "touch {IMAGENAME}"
     t.on_success = None
+
+
+@pytest.mark.skip()
+def test_CLGrader_on_fail_callback():
+  g = CLGrader()
+
 
   with a.add_test() as t:
     t.NS.SCRIPTNAME = "cos.gnuplot"
@@ -39,10 +82,14 @@ def test_CLGrader():
         ff.commands += '''gnuplot -e 'set term png; set ouptut "{IMAGENAME}"' $(find_file "{SCRIPTNAME}")'''
 
 
+@pytest.mark.skip()
+def test_CLGrader_basic():
+  g = CLGrader()
+
+
   with a.add_test() as t:
     t.commands += "test -f {IMAGENAME}"
     t.run_if = cmd("test -e {IMAGENAME}")
 
-  g.run()
 
 
