5b8c90deb28badcc0cd4f02628ef9b70cb91939e
C.D. Clark III <clifton.clark@gmail.com>
2018-04-27 00:08:34 -0500

    added support for running unit tests in docker container.

---
+++ testing/docker/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker build -t pyassignment-testing-3.6 -f Dockerfile-pyassignment-testing-3.6 .
---
+++ util-scripts/run_tests.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker run -v $PWD:/home/testenv pyassignment-testing-3.6:latest
